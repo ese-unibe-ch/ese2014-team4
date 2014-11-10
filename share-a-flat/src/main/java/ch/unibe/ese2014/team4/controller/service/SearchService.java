a808ea76c5ae6bce1644f1b3446fc2bb00b1181c
Tanja Kuery <tanja.kuery@students.unibe.ch>
2014-11-11 00:33:51 +0100

    some more changes to search list

---
+++ share-a-flat/src/main/java/ch/unibe/ese2014/team4/controller/service/SearchService.java
@@ -0,0 +1,27 @@
+package ch.unibe.ese2014.team4.controller.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import ch.unibe.ese2014.team4.controller.exceptions.InvalidUserException;
+import ch.unibe.ese2014.team4.controller.pojos.AdForm;
+import ch.unibe.ese2014.team4.model.Ad;
+import ch.unibe.ese2014.team4.model.User;
+import ch.unibe.ese2014.team4.model.dao.AdDao;
+
+public interface AdService {
+
+	public AdForm saveAdForm(AdForm adForm, User owner) throws InvalidUserException, Exception;
+	public Ad getAd(Long id);
+	public Collection<Ad> getNewestAds(int days);
+	public List<Ad> getAdByPrice(int price);
+	public List<Ad> getAdByTitle(String title);
+	public List<Ad> getAdByCity(String city);
+	public void setAdDao(AdDao mockDao);
+
+
+
+}
