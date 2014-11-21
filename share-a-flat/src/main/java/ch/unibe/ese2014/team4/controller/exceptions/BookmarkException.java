fac44d602575ef1ad1fc1a09714ece69d292a93a
bachtler <m.bachtler@students.unibe.ch>
2014-11-21 01:07:12 +0100

    added file

---
+++ share-a-flat/src/main/java/ch/unibe/ese2014/team4/controller/exceptions/BookmarkException.java
@@ -0,0 +1,8 @@
+package ch.unibe.ese2014.team4.controller.exceptions;
+
+public class BookmarkException extends RuntimeException {
+
+    public BookmarkException(String s) {
+        super(s);
+    }
+}
