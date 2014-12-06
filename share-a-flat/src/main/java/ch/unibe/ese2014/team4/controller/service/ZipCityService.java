493431e0905b1ca22660d2e9cdee4d6caa22d4c4
bachtler <m.bachtler@students.unibe.ch>
2014-12-06 19:26:08 +0100

    added file

---
+++ share-a-flat/src/main/java/ch/unibe/ese2014/team4/controller/service/ZipCityService.java
@@ -0,0 +1,11 @@
+package ch.unibe.ese2014.team4.controller.service;
+
+import java.util.List;
+
+import ch.unibe.ese2014.team4.controller.service.ZipCityServiceImpl.ZipCity;
+
+public interface ZipCityService {
+
+	public List<ZipCity> getZipCityAsList();
+
+}
